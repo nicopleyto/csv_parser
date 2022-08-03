079af35b53d8d351b44cedbd59af51e68cc73698
nicopleyto <nicopleyto@gmail.com>
2022-08-03 18:03:36 +0800

    initial commit

---
+++ csv_parser.py
@@ -0,0 +1,29 @@
+import csv
+
+#initializes the output file with headers
+outfile = open("parsed_data.csv", 'w')
+outfile_header = "First Name,Last Name,Position,Company Email,Mobile Number,Originating URL Submitted on,Form Location,Status,Submitted on\n"
+outfile.write(outfile_header)
+
+#reads the csv file to be parsed
+with open("logs-insights-results (1).csv", 'r') as infile:
+  reader = csv.reader(infile)
+  next(reader) #skips header of file to be read
+  for row in reader:
+    raw_data = row[0]
+
+    # first_name =
+    # last_name =
+    # position =
+    # email =
+    # mobile =
+    # origin =
+    # form_location =
+    # status =
+    # submitted_on =
+
+
+    # line = "{},{},{},{},{},{},{},{},{}\n".format(first_name, last_name, position, email, mobile, origin, form_location, status, submitted_on)
+    # outfile.write(line)
+
+outfile.close
