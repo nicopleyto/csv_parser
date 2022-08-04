72177ed23449682ba65178b241d365a8f3ad5c43
nicopleyto <nicopleyto@gmail.com>
2022-08-04 18:04:06 +0800

    finished csv parser, added string to datetime converter

--- csv_parser.py
+++ csv_parser.py
@@ -1,9 +1,10 @@
 import csv
 import re
+from datetime import datetime
 
 #initializes the output file with headers
 outfile = open("parsed_data.csv", 'w')
-outfile_header = "First Name,Last Name,Position,Company Email,Mobile Number,Originating URL Submitted on,Form Location,Status,Submitted on\n"
+outfile_header = "First Name,Last Name,Position,Company Email,Mobile Number,Originating URL Submitted on,Form Location,Submitted on\n"
 outfile.write(outfile_header)
 
 #reads the csv file to be parsed
@@ -12,18 +13,19 @@ with open("logs-insights-results (1).csv", 'r') as infile:
   next(reader) #skips header of file to be read
   for row in reader:
     raw_data = row[0]
+
     first_name = re.compile(r'(?<=\"first_name\"=>\")(.*?)(?=\")').search(raw_data).group()
-    # last_name =
-    # position =
-    # email =
-    # mobile =
-    # origin =
-    # form_location =
-    # status =
-    # submitted_on =
-    # print(raw_data)
-    line = "{}\n".format(first_name)
-    # line = "{},{},{},{},{},{},{},{},{}\n".format(first_name, last_name, position, email, mobile, origin, form_location, status, submitted_on)
+    last_name = re.compile(r'(?<=\"last_name\"=>\")(.*?)(?=\")').search(raw_data).group()
+    position = re.compile(r'(?<=\"position\"=>\")(.*?)(?=\")').search(raw_data).group()
+    email = re.compile(r'(?<=\"email\"=>\")(.*?)(?=\")').search(raw_data).group()
+    mobile = re.compile(r'(?<=\"mobile\"=>\")(.*?)(?=\")').search(raw_data).group()
+    origin_url = re.compile(r'(?<=\"url\"=>\")(.*?)(?=\")').search(raw_data).group()
+    form_location = re.compile(r'(?<=\"form_location\"=>\")(.*?)(?=\")').search(raw_data).group()
+
+    submitted_on_string = re.compile(r'(?<=I, \[)(.*?)(?=#)').search(raw_data).group().strip()
+    submitted_on = datetime.fromisoformat(submitted_on_string) #convert string to datetime object
+
+    line = "{},{},{},{},{},{},{},{}\n".format(first_name, last_name, position, email, mobile, origin_url, form_location, submitted_on)
     outfile.write(line)
 
 outfile.close
